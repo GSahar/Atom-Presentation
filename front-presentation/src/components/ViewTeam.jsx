2dcb0c6fa2db2203246b10ae171f5d2175b6c5e7
GSahar <andrey_144_sahar@mail.ru>
2022-09-06 20:03:56 +0500

    Доработки в фронте и в данных

--- front-presentation/src/components/ViewTeam.jsx
+++ front-presentation/src/components/ViewTeam.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 const ListItem = ({ value }) => (
-  <div className='div_li'>
-    <div className='li_img'>
-      <img className='li_avatar' src={value.Avatar} alt='Иконка чемпионата' />
+  <div className="div_li">
+    <div className="li_img">
+      <img className="li_avatar" src={value.Avatar} alt="Иконка чемпионата" />
     </div>
 
-    <dev className='li_content'>
-      <div className='li_title'>{value.Title}</div>
-      <div className='li_description'>{value.Description}</div>
+    <dev className="li_content">
+      <div className="li_title">{value.Title}</div>
+      <div className="li_description">{value.Description}</div>
     </dev>
   </div>
 );
 
 const List = ({ items }) => (
-  <div className='div_ul'>
-    {
-      items.map((item, i) => <ListItem key={i} value={item} />)
-    }
+  <div className="div_ul">
+    {items.map((item, i) => (
+      <ListItem key={i} value={item} />
+    ))}
   </div>
 );
 
@@ -38,23 +38,48 @@ export default class ViewTeam extends Component {
 
     let { Title, Team, Description, Competency, Avatar } = newData[0];
 
-    return (
-      <div className='container-team'>
-        <div className='head-container'>
+    /*  */
 
-          <div className='container-content'>
-            <h2 className='head-text'>{Competency}</h2>
-            <h1 className='container-title'>{Title}</h1>
-          </div>
-
-          <div className='container-img-content'>
-            <img className='head-img' src={Avatar} alt='Иконка чемпионата' />
-          </div>
+    /* 
+      <div className="head-container">
+          <h2 className="head-text">{Competency}</h2>
         </div>
 
+        <div className="body-container">
+          <div className="container-content">
+            <div className="left-panel">
+              <div className="container-img-content">
+                <img className="head-img" src={Avatar} alt="Иконка чемпионата" />
+              </div>
+              <h1 className="container-title">{Title}</h1>
+            </div>
+            <div className="Right-panel">
+              <List items={Team} />
+              <div className="container-desc">{Description} </div>
+            </div>
+          </div>
+        </div>
+      */
 
-        <List items={Team} />
-        <div className='container-desc'>{Description} </div>
+    return (
+      <div className="container-team">
+        <div className="head-container">
+          <h2 className="head-text">{Competency}</h2>
+        </div>
+
+        <div className="container-content">
+          <div className="left-panel">
+            <div className="container-img-content">
+              <img className="head-img" src={Avatar} alt="Иконка чемпионата" />
+            </div>
+            <h1 className="container-title">{Title}</h1>
+          </div>
+
+          <div className="right-panel">
+            <List items={Team} />
+            <div className="container-desc">{Description} </div>
+          </div>
+        </div>
       </div>
     );
   }
